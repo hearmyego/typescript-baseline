50c7790c69ef60afccc39436a254ebee6bd173d6
Michael Brandt <mib@hearmyego.com>
2021-03-22 00:41:51 +0100

    Adding template resolver

--- src/controllers/defaultController.ts
+++ src/controllers/defaultController.ts
@@ -3,6 +3,7 @@ import baseController from './baseController';
 import { Methods } from '../core/Methods';
 
 import { IContentPayload } from '../core/IContentPayload';
+import getTemplate from '../helpers/templateHelper';
 
 export default class defaultController extends baseController {
 	public path = '/'; // The path on which this.routes will be mapped
@@ -56,7 +57,7 @@ export default class defaultController extends baseController {
 	async slug(request: Request, response: Response): Promise<void> {
 		console.log('slug');
 		const slug = request.params.slug;
-		console.log(slug);
-		response.status(200).render('pages/' + slug);
+		const template = getTemplate(slug);
+		response.status(template.status).render(template.templatename);
 	}
 }

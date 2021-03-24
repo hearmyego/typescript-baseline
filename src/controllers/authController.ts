918ea143e1b0c576cfecbdac790ae354349126e0
Michael Brandt <mib@hearmyego.com>
2021-03-24 08:20:50 +0100

    Refactor local login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,31 +1,38 @@
 import { Response, Request, NextFunction, RequestHandler } from 'express';
 import baseController from './baseController';
 import { Methods } from '../core/Methods';
+
 import passport from 'passport';
 
 export default class authController extends baseController {
-	public path = '/';
+	public path = '/local';
 	public routes = [
 		{
 			path: '/logout',
 			method: Methods.GET,
-			handler: this.logout,
+			handler: this.logoutGet,
 			localMiddleware: [],
 		},
 		{
-			path: '/login', // Will become /:slug/<<VALUE>>
+			path: '/login',
 			method: Methods.POST,
 			handler: this.loginPost,
 			localMiddleware: [
 				passport.authenticate('local-login', {
-					successRedirect: '/profile', // redirect to the secure profile section
-					failureRedirect: '/login', // redirect back to the signup page if there is an error
-					failureFlash: false, // allow flash messages
+					successRedirect: '/profile',
+					failureRedirect: '/local/login',
+					failureFlash: false,
 				}),
 			],
 		},
 		{
-			path: '/signup',
+			path: '/login',
+			method: Methods.GET,
+			handler: this.loginGet,
+			localMiddleware: [],
+		},
+		{
+			path: 'signup',
 			method: Methods.POST,
 			handler: this.signupPost,
 			localMiddleware: [
@@ -43,12 +50,17 @@ export default class authController extends baseController {
 		response.status(200).render('pages/login');
 	}
 
+	async loginGet(request: Request, response: Response): Promise<void> {
+		console.log('loginGet');
+		response.status(200).render('pages/login');
+	}
+
 	async signupPost(request: Request, response: Response): Promise<void> {
 		console.log('signupPost');
 		response.status(200).render('pages/signup');
 	}
 
-	async logout(request: Request, response: Response): Promise<void> {
+	async logoutGet(request: Request, response: Response): Promise<void> {
 		console.log('logout');
 		request.logout();
 		response.redirect('/');

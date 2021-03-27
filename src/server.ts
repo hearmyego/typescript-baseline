e1a02822742656e3c1e22ae7301971140aea1c11
Michael Brandt <mib@hearmyego.com>
2021-03-27 15:35:29 +0100

    Adding cluster

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
+import cluster from 'cluster';
+import os from 'os';
 import App from './app';
 
+const PORT = parseInt(process.env.PORT || '3000');
 require('dotenv').config();
 
-const PORT = parseInt(process.env.PORT || '3000');
-const app = new App(PORT);
-
-app.listen();
+if (cluster.isMaster) {
+	for (let index = 0; index < os.cpus().length; index++) {
+		cluster.fork();
+	}
+	cluster.on('exit', (worker) => {
+		cluster.fork();
+	});
+} else {
+	const app = new App(PORT);
+	app.listen();
+}

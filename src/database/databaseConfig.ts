d24696986254684f4aa753e59fd8f8b6dd3caffd
Michael Brandt <mib@hearmyego.com>
2021-03-27 15:34:54 +0100

    Adding .env file

--- src/database/databaseConfig.ts
+++ src/database/databaseConfig.ts
@@ -1,4 +1,3 @@
 export default {
-	url:
-		'mongodb+srv://lmsstage:D6X9iRB0u2LA4d7g@cluster0.ezon5.mongodb.net/lms-stage?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	url: process.env.MONGODB || '',
 };

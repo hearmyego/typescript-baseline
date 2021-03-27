4cceaa506b7f58a0705721eafa78fdc2e183ce82
Michael Brandt <mib@hearmyego.com>
2021-03-27 16:30:36 +0100

    Configurabel numbers of workers

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import cluster from 'cluster';
 import os from 'os';
 import App from './app';
 
-const PORT = parseInt(process.env.PORT || '3000');
 require('dotenv').config();
 
+const PORT = parseInt(process.env.PORT || '3000');
+const numberOfWorkers = parseInt(
+	process.env.NumberOfWorkers || os.cpus().length.toString()
+);
+
 if (cluster.isMaster) {
-	for (let index = 0; index < os.cpus().length; index++) {
+	for (let index = 0; index < numberOfWorkers; index++) {
 		cluster.fork();
 	}
 	cluster.on('exit', (worker) => {
